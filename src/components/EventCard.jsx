73eed893435b538cc4eb2d983b0e9f7d4eba5c7c
Julie <juth0001@stud.kea.dk>
2026-05-04 11:57:30 +0200

    label component

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,51 +1,48 @@
 import Image from "next/image";
 import Link from "next/link";
+import Label from "@/components/Label";
 
 const EventCard = ({ id, image, title, date, time, location, building, price }) => {
   return (
-<Link href={`/arrangment/${id}`} className="block">
-    <article className="mb-6 border-b border-neutral-200 pb-6">
-      <div className="mx-auto mb-2 flex w-[90%] items-center justify-between">
-        <small className="rounded bg-white/90 px-2 py-1">{date}</small>
+    <Link href={`/arrangment/${id}`} className="block">
+      <article className="mb-6 border-b border-neutral-200 pb-6">
+        <div className="mx-auto mb-2 flex w-[90%] items-center justify-between">
+          <small className="rounded bg-white/90 px-2 py-1">{date}</small>
 
-        <small className="rounded-md bg-yellow-300 px-3 py-1 uppercase">
-          {building || "Lokation"}
-        </small>
-      </div>
-
-      <div className="relative mx-auto h-[220px] w-[90%] overflow-hidden rounded-2xl">
-        {image && (
-          <Image
-            src={image}
-            alt={title || "Event"}
-            fill
-            className="object-cover"
-          />
-        )}
-      </div>
-
-      <div className="mx-auto mt-3 w-[90%]">
-        <h3 className="mb-2">{title}</h3>
-
-        <div className="flex items-end justify-between">
-          <div>
-            <small>
-              {time} · {date} ·  {location}
-            </small>
-           
-          </div>
-
-          
+          <Label variant="location">
+            {building || "Lokation"}
+          </Label>
         </div>
-        <div className="mx-auto mb-2 flex w-[100%] items-center justify-between">
-        {price && <small className="mt-1 block">{price}</small>}
-        <button className="underline underline-offset-4">LÆS MERE</button>
-      </div>
-      </div>
-    </article>
+
+        <div className="relative mx-auto h-[220px] w-[90%] overflow-hidden rounded-2xl">
+          {image && (
+            <Image
+              src={image}
+              alt={title || "Event"}
+              fill
+              className="object-cover"
+            />
+          )}
+        </div>
+
+        <div className="mx-auto mt-3 w-[90%]">
+          <h3 className="mb-2">{title}</h3>
+
+          <small>
+            {time} · {date} · {location}
+          </small>
+
+          <div className="mt-2 flex w-full items-center justify-between">
+            {price && <small>{price}</small>}
+
+            <span className="button-text underline underline-offset-4">
+              Læs mere
+            </span>
+          </div>
+        </div>
+      </article>
     </Link>
   );
 };
-
 
 export default EventCard;
